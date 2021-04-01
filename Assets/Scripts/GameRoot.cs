c2b0eec7af8adc8140e71e920f9782a4b8a97aac
ashishfilu <81488951+ashishfilu@users.noreply.github.com>
2021-04-01 18:01:20 +0100

    Selection of Domino is added
    1. User can select the domino
    2. Left mouse to spawn a new domino
    3. Right click to apply force

--- Assets/Scripts/GameRoot.cs
+++ Assets/Scripts/GameRoot.cs
@@ -2,14 +2,29 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum DominoType
+{
+    Red,
+    Green
+};
+
 public class GameRoot : MonoBehaviour
 {
+    public GameObject DominosRoot;
+
     private RaycastHit mHitObject;
+    private DominoType mDominoType = DominoType.Green;
+    private GameObject mGreenDomino;
+    private GameObject mRedDomino;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        GameEventManager.Instance.SubscribeEventListener(EventId.GreenButtonPressed, ProcessButtonPress);
+        GameEventManager.Instance.SubscribeEventListener(EventId.RedButtonPressed, ProcessButtonPress);
+
+        mGreenDomino = Resources.Load<GameObject>("Prefabs/DominoCube_Green");
+        mRedDomino = Resources.Load<GameObject>("Prefabs/DominoCube_Red");
     }
 
     // Update is called once per frame
@@ -21,8 +36,43 @@ public class GameRoot : MonoBehaviour
             if( Physics.Raycast(ray,out mHitObject))
             {
                 Rigidbody rigidbody = mHitObject.rigidbody;
-                rigidbody.AddForce(ray.direction * 12, ForceMode.Impulse);
+                rigidbody?.AddForce(ray.direction * 12, ForceMode.Impulse);
             }
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out mHitObject))
+            {
+                if( mHitObject.transform.gameObject.CompareTag("Ground"))
+                {
+                    GameObject temp = null;
+                    if ( mDominoType == DominoType.Green )
+                    {
+                        temp = GameObject.Instantiate(mGreenDomino);
+                    }
+                    else
+                    {
+                        temp = GameObject.Instantiate(mRedDomino);
+                    }
+
+                    temp.transform.parent = DominosRoot.transform;
+                    temp.transform.position = mHitObject.point;
+                }
+            }
+        }
+    }
+
+    void ProcessButtonPress(object data)
+    {
+        if(data.ToString() == EventId.GreenButtonPressed )
+        {
+            mDominoType = DominoType.Green;
+        }
+        else if( data.ToString() == EventId.RedButtonPressed )
+        {
+            mDominoType = DominoType.Red;
         }
     }
 }
---
+++ Assets/Scripts/HUD.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HUD : MonoBehaviour
+{
+    public Button GreenButton;
+    public Button RedButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GreenButton.onClick.AddListener(OnGreenButtonClicked);
+        RedButton.onClick.AddListener(OnRedButtonClicked);
+    }
+
+    void OnGreenButtonClicked()
+    {
+        GameEventManager.Instance.TriggerEvent(EventId.GreenButtonPressed , EventId.GreenButtonPressed);
+    }
+
+    void OnRedButtonClicked()
+    {
+        GameEventManager.Instance.TriggerEvent(EventId.RedButtonPressed,EventId.RedButtonPressed);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+}
+
+public partial class EventId
+{
+    public static string GreenButtonPressed = "GreenButtonPressed";
+    public static string RedButtonPressed = "RedButtonPressed";
+}
